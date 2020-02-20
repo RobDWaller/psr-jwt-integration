b51681fdbf06980adb18bd03bbeaf9b297d7fd06
RobDWaller <rdwaller1984@googlemail.com>
2020-02-20 07:28:25 +0000

    Added advanced routes to test basic token validation.

--- public/index.php
+++ public/index.php
@@ -20,6 +20,7 @@ $app->get('/', function (Request $request, Response $response) {
 
     $token = $builder->setSecret('!secReT$123*')
         ->setPayloadClaim('uid', 12)
+        ->setExpiration(time() + 600)
         ->build();
 
     $tokenString = $token->getToken();
@@ -27,6 +28,8 @@ $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("<p>" . $tokenString . "<p>");
     
     $response->getBody()->write('<p><a href="/token/read?jwt=' . $tokenString . '">Read Token</a></p>');
+
+    $response->getBody()->write('<p><a href="/advanced">Advanced</a></p>');
 
     return $response;
 });
@@ -54,5 +57,50 @@ $app->get('/token/read', function (Request $request, Response $response) {
 
     return $response;
 });
+
+$app->get('/advanced', function (Request $request, Response $response) {
+    
+    $response->getBody()->write('<h1>Advanced</h1>');
+
+    $factory1 = new \PsrJwt\Factory\Jwt();
+
+    $builder1 = $factory1->builder();
+
+    $token1 = $builder1->setSecret('!secReT$123*')
+        ->setPayloadClaim('uid', 12)
+        ->setExpiration(time() + 600)
+        ->build();
+
+    $tokenString1 = $token1->getToken();
+
+    $response->getBody()->write("<p>" . $tokenString1 . "<p>");
+    
+    $response->getBody()->write('<p><a href="/advanced/validate?jwt=' . $tokenString1 . '">Validate Success</a></p>');
+
+    $factory2 = new \PsrJwt\Factory\Jwt();
+
+    $builder2 = $factory2->builder();
+
+    $token2 = $builder2->setSecret('!secReT$123*')
+        ->setPayloadClaim('uid', 12)
+        ->setPayloadClaim('exp', time() - 600)
+        ->build();
+
+    $tokenString2 = $token2->getToken();
+
+    $response->getBody()->write("<p>" . $tokenString2 . "<p>");
+    
+    $response->getBody()->write('<p><a href="/advanced/validate?jwt=' . $tokenString2 . '">Validate Failure</a></p>');
+    
+    $response->getBody()->write('<p><a href="/">Back</a></p>');
+    
+    return $response;
+});
+
+$app->get('/advanced/validate', function (Request $request, Response $response) {
+    $response->getBody()->write("<h1>OK!</h1>");
+    $response->getBody()->write('<p><a href="/advanced">Back</a></p>');
+    return $response;
+})->add(\PsrJwt\Factory\JwtMiddleware::html('!secReT$123*', 'jwt', '<h1>Fail!!</h1><p><a href="/advanced">Back</a></p>'));
 
 $app->run();
